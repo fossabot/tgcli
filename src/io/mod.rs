2ab441499518338ce8d2b8b057c5a2f5e85e806e
Eray Erdin <eraygezer.94@gmail.com>
2021-11-30 10:24:08 +0300

    created io::fidc module

--- src/io/mod.rs
+++ src/io/mod.rs
@@ -12,19 +12,4 @@
 // See the License for the specific language governing permissions and
 // limitations under the License.
 
-#[macro_use]
-extern crate log;
-
-#[macro_use]
-extern crate serde;
-
-#[macro_use]
-extern crate async_trait;
-
-pub mod cli;
-pub mod convert;
-mod http;
-mod io;
-pub mod operations;
-
-const API_ROOT_URL: &str = "https://api.telegram.org/bot";
+mod fidc;
